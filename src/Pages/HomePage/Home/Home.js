2d81d5ee78934703e05252445ff7370a80e67b80
MD. ARIFUL HASAN TOTUL <mdahtotul@gmail.com>
2021-12-01 09:52:07 +0600

    res

--- src/Pages/HomePage/Home/Home.js
+++ src/Pages/HomePage/Home/Home.js
@@ -1,4 +1,5 @@
 import { Container } from "@mui/material";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import React from "react";
 import HomeProjects from "../HomeProjects/HomeProjects";
 import HomeSection from "../HomeSection/HomeSection";
@@ -7,8 +8,23 @@ import "./Home.css";
 const Home = () => {
    return (
       <Container className="homepage">
-         <HomeSection />
-         <HomeProjects />
+         <Parallax pages={2} style={{ top: "0", left: "0" }}>
+            <ParallaxLayer
+               offset={0}
+               speed={2.5}
+               style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+               }}
+            >
+               <HomeSection />
+            </ParallaxLayer>
+
+            <ParallaxLayer offset={1} speed={2}>
+               <HomeProjects />
+            </ParallaxLayer>
+         </Parallax>
       </Container>
    );
 };
