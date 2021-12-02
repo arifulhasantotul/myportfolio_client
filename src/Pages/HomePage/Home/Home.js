ac1d9bee14a2643ee1a8ca60c762f322504b2151
MD. ARIFUL HASAN TOTUL <mdahtotul@gmail.com>
2021-12-02 20:31:37 +0600

    parallax removed

--- src/Pages/HomePage/Home/Home.js
+++ src/Pages/HomePage/Home/Home.js
@@ -1,5 +1,4 @@
 import { Container } from "@mui/material";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import React from "react";
 import HomeProjects from "../HomeProjects/HomeProjects";
 import HomeSection from "../HomeSection/HomeSection";
@@ -8,23 +7,8 @@ import "./Home.css";
 const Home = () => {
    return (
       <Container className="homepage">
-         <Parallax pages={2} style={{ top: "0", left: "0" }}>
-            <ParallaxLayer
-               offset={0}
-               speed={2.5}
-               style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-               }}
-            >
-               <HomeSection />
-            </ParallaxLayer>
-
-            <ParallaxLayer offset={1} speed={2}>
-               <HomeProjects />
-            </ParallaxLayer>
-         </Parallax>
+         <HomeSection />
+         <HomeProjects />
       </Container>
    );
 };
